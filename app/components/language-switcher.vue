62943477e9d642e9ded432437a08499f4a85bfbc
George Sysuev <no1621name@gmail.com>
2026-05-03 13:25:28 +0300

    feat(i18n): add language switcher component

---
+++ app/components/language-switcher.vue
@@ -0,0 +1,21 @@
+<script setup lang="ts">
+const { locale, locales } = useI18n();
+const switchLocalePath = useSwitchLocalePath();
+
+const availableLocales = computed(() => {
+  return locales.value.filter(i => i.code !== locale.value);
+});
+</script>
+
+<template>
+  <UButton
+    v-for="availableLocale in availableLocales"
+    :key="availableLocale.code"
+    :to="switchLocalePath(availableLocale.code)"
+    variant="ghost"
+    size="sm"
+    color="primary"
+  >
+    {{ availableLocale.code }}
+  </UButton>
+</template>
