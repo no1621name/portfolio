12ce37a3f2ec5305cb945b747c654e756a10a76a
George Sysuev <no1621name@gmail.com>
2026-05-03 13:24:44 +0300

    feat(content): change slug page content loading to follow new content structure

--- app/pages/[...slug].vue
+++ app/pages/[...slug].vue
@@ -1,9 +1,19 @@
 <script setup lang="ts">
-const route = useRoute();
+import type { Collections } from '@nuxt/content';
 
-const { data: page } = await useAsyncData('page-' + route.path, () => {
-  return queryCollection('pages').path('/pages' + route.path).first();
+const route = useRoute();
+const { locale, defaultLocale } = useI18n();
+
+const slug = computed(() => Array.isArray(route.params.slug) ? route.params.slug as string[] : [route.params.slug as string]);
+const collection = computed(() => `content_${locale.value}` as keyof Collections);
+const path = computed(() => {
+  const localePrefix = locale.value === defaultLocale ? '' : `/${locale.value}`;
+
+  return `${localePrefix}/pages/${slug.value}`;
 });
+const { data: page } = await useAsyncData('page-' + route.path, () =>
+  queryCollection(collection.value).path(path.value).first()
+);
 
 if (!page.value) {
   throw createError({ statusCode: 404, statusMessage: 'Page not found', fatal: true });
