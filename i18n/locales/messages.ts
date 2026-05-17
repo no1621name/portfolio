dd93ea817071c4284e6ea1204258b17507816f02
George Sysuev <no1621name@gmail.com>
2026-05-17 19:27:06 +0300

    feat(i18n): add experience translations

--- i18n/locales/messages.ts
+++ i18n/locales/messages.ts
@@ -1,9 +1,23 @@
 const messages = {
   en: {
-    themeSwitcher: 'Theme switcher'
+    themeSwitcher: 'Theme switcher',
+    experience: {
+      present: 'present',
+      year: 'yr',
+      month: 'mo',
+      seeMore: 'see more',
+      seeLess: 'see less'
+    }
   },
   ru: {
-    themeSwitcher: 'Смена темы'
+    themeSwitcher: 'Смена темы',
+    experience: {
+      present: 'настоящее время',
+      year: 'г.',
+      month: 'мес.',
+      seeMore: 'смотреть больше',
+      seeLess: 'свернуть'
+    }
   }
 };
 
